4e11a4d21a73228ad7c8bfd62092217e60ebb5db
Ellis Kenyo <elken@mykolab.com>
2015-12-25 13:52:59 +0000

    Finished save slot

--- Window.py
+++ Window.py
@@ -1,5 +1,5 @@
 from PyQt4.Qt import pyqtSlot
-from PyQt4.QtCore import QSettings
+from PyQt4.QtCore import QSettings, Qt
 from PyQt4.QtGui import *
 
 
@@ -17,13 +17,14 @@ class SempyConfig(QDialog):
 
         self.reload_button = QPushButton("Reload")
         self.save_button = QPushButton("Save")
-        self.refresh_button = QPushButton("Refresh repos")
+        self.refresh_button = QPushButton("Refresh")
         self.verify_button = QPushButton("Verify")
 
         self.box_layout = QGridLayout()
         self.main_layout = QGridLayout()
         self.box_group = QButtonGroup()
         self.btn_layout = QHBoxLayout()
+        self.settings_layout = QGridLayout()
 
         self.interval_spinner = QSpinBox()
         self.interval_label = QLabel("Interval: ")
@@ -33,6 +34,7 @@ class SempyConfig(QDialog):
         self.init_ui()
 
     def init_ui(self):
+        """ Create all the UI elements """
         self.box_group.setExclusive(False)
         self.interval_spinner.setSuffix(" seconds")
         self.interval_spinner.setValue(int(self.settings.value("interval")))
@@ -40,21 +42,21 @@ class SempyConfig(QDialog):
         self.setWindowTitle('Sempy Configuration')
         self.setWindowIcon(QIcon('res/semaphore.png'))
         self.interval_label.setBuddy(self.interval_spinner)
-        row = 0
+        self.box_layout.addWidget(QLabel("<b>Enable/disable repositories below:</b>"), 0, 0)
+        row = 1
+
         self.settings.beginGroup("Repositories")
         for key in self.settings.allKeys():
             self.info[key] = self.settings.value(key)
 
         for key, val in self.info.items():
-            checkbox = QCheckBox(key, self)
+            checkbox = QCheckBox(key)
+            if val == "True":
+                checkbox.setCheckState(Qt.Checked)
             self.box_layout.addWidget(checkbox, row, 0)
             self.box_group.addButton(checkbox)
-            label = QLabel()
-            # icon = QIcon("res/" + val['result'] + ".svg")
-            icon = QIcon("res/passed.svg")
-            label.setPixmap(icon.pixmap(8))
-            self.box_layout.addWidget(label, row, 1)
             row += 1
+        self.settings.endGroup()
 
         self.save_button.clicked.connect(self.save)
         self.reload_button.clicked.connect(self.reload)
@@ -63,26 +65,34 @@ class SempyConfig(QDialog):
         self.btn_layout.addWidget(self.save_button)
         self.btn_layout.addWidget(self.reload_button)
 
-        self.main_layout.addWidget(self.interval_label, 0, 0)
-        self.main_layout.addWidget(self.interval_spinner, 0, 1)
-        self.main_layout.addWidget(self.token_label, 1, 0)
-        self.main_layout.addWidget(self.token_box, 1, 1)
-        self.main_layout.addWidget(self.refresh_button, 1, 2)
-        self.main_layout.addWidget(self.verify_button, 1, 3)
-        self.main_layout.addWidget(QLabel("<b>Enable/disable repositories below:</b>"), 2, 0)
-        self.main_layout.addItem(self.box_layout, 3, 0)
-        # self.main_layout.addItem(self.btn_layout, 4, 0)
-        self.main_layout.addWidget(self.save_button, 4, 0)
-        self.main_layout.addWidget(self.reload_button, 4, 1)
+        self.settings_layout.addWidget(self.interval_label, 0, 0)
+        self.settings_layout.addWidget(self.interval_spinner, 0, 1)
+        self.settings_layout.addWidget(self.token_label, 1, 0)
+        self.settings_layout.addWidget(self.token_box, 1, 1)
+        self.settings_layout.addWidget(self.refresh_button, 1, 2)
+        self.settings_layout.addWidget(self.verify_button, 1, 3)
+
+        self.main_layout.addItem(self.settings_layout, 0, 0)
+        self.main_layout.addItem(self.box_layout, 2, 0)
+        self.main_layout.addItem(self.btn_layout, 4, 0)
+
         self.setLayout(self.main_layout)
 
     @pyqtSlot()
     def save(self):
-        print("save")
+        self.settings.setValue("interval", self.interval_spinner.value())
+        self.settings.setValue("token", self.token_box.text())
+        self.settings.beginGroup("Repositories")
+        for i in self.box_group.buttons():
+            self.settings.setValue(i.text(), str(i.isChecked()).capitalize())
+        self.settings.endGroup()
 
     @pyqtSlot()
     def reload(self):
-        print("reload")
+        self.interval_spinner.setValue(int(self.settings.value("interval")))
+        self.token_box.setText(self.settings.value("token"))
+        for i in self.box_group.buttons():
+            i.setCheckState(self.settings.value(i.text()))
 
     @pyqtSlot()
     def verify(self):
